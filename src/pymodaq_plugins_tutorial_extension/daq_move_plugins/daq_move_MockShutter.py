2a38a7c027e05f1be663e7c62d225f15b2c9a148
Bernhard Lang <bernhard.lang@unige.ch>
2026-04-02 13:14:32 +0200

    shutter

--- src/pymodaq_plugins_tutorial_extension/daq_move_plugins/daq_move_MockShutter.py
+++ src/pymodaq_plugins_tutorial_extension/daq_move_plugins/daq_move_MockShutter.py
@@ -7,15 +7,7 @@ from pymodaq_plugins_tutorial_extension.hardware.controller \
 
 
 class DAQ_Move_MockShutter(DAQ_Move_base):
-    """ Instrument plugin class for an actuator.
-    
-    Attributes:
-    -----------
-    controller: object
-        The particular object that allow the communication with the hardware,
-        in general a python wrapper around the hardware library.
 
-    """
     is_multiaxes = True
     _axis_names = MockSpectrograph.shutter_names[:2]
     _controller_units = '' #: Union[str, List[str]] = ['mm', 'mm']
@@ -29,12 +21,6 @@ class DAQ_Move_MockShutter(DAQ_Move_base):
         self.controller: MockTAController = None
 
     def get_actuator_value(self):
-        """Get the current value from the hardware with scaling conversion.
-
-        Returns
-        -------
-        float: The position obtained after scaling conversion.
-        """
         axis = self.settings['multiaxes', 'axis']
         pos = DataActuator(data=self.controller.get_shutter_value(axis),
                            units=self.axis_unit)
@@ -42,35 +28,12 @@ class DAQ_Move_MockShutter(DAQ_Move_base):
         return pos
 
     def close(self):
-        """Terminate the communication protocol"""
         pass
 
     def commit_settings(self, param: Parameter):
-        """Apply the consequences of a change of value in the detector settings
-
-        Parameters
-        ----------
-        param: Parameter
-            A given parameter (within detector_settings) whose value has been
-            changed by the user
-        """
         pass
 
     def ini_stage(self, controller=None):
-        """Actuator communication initialization
-
-        Parameters
-        ----------
-        controller: (object)
-            custom object of a PyMoDAQ plugin (Slave case). None if only one
-            actuator by controller (Master case)
-
-        Returns
-        -------
-        info: str
-        initialized: bool
-            False if initialization failed otherwise True
-        """
         if self.is_master:
             self.controller = MockSpectrograph()
         else:
@@ -80,12 +43,6 @@ class DAQ_Move_MockShutter(DAQ_Move_base):
         return info, True
 
     def move_abs(self, value: DataActuator):
-        """ Move the actuator to the absolute target defined by value
-
-        Parameters
-        ----------
-        value: (float) value of the absolute target positioning
-        """
         value = self.check_bound(value)
         self.target_value = value
         value = self.set_position_with_scaling(value)
@@ -95,30 +52,14 @@ class DAQ_Move_MockShutter(DAQ_Move_base):
                                        ['Moved shutter %s' % axis]))
 
     def move_rel(self, value: DataActuator):
-        """ Move the actuator to the relative target actuator value defined
-            by value
-
-        Parameters
-        ----------
-        value: (float) value of the relative target positioning
-        """
         axis = self.settings['multiaxes', 'axis']
-        current_position = self.get_actuator_value(axis)
-        value = self.check_bound(current_position + value) - current_position
-        self.target_value = value + current_position
-        value = self.set_position_relative_with_scaling(value)
-
-        self.controller.delay_line.move_at(axis, value.value(self.axis_unit))
-        self.emit_status(ThreadCommand('Update_Status',
-                                       ['Moved shutter %s' % axis]))
+        self.move_abs(self.get_actuator_value(axis) + value)
 
     def move_home(self):
-        """Call the reference method of the controller"""
         self.emit_status(ThreadCommand('Update_Status',
                                        ['Move Home not implemented']))
 
     def stop_motion(self):
-        """Stop the actuator and emits move_done signal"""
         self.move_done()
 
 
