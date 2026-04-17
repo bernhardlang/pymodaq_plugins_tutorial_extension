67b879d3b2641b418540ef9eb3766e1998ca0289
Bernhard Lang <bernhard.lang@unige.ch>
2026-04-17 16:31:06 +0200

    before absorption

--- src/pymodaq_plugins_tutorial_extension/extensions/absorption_extension.py
+++ src/pymodaq_plugins_tutorial_extension/extensions/absorption_extension.py
@@ -1,3 +1,4 @@
+import numpy as np
 from qtpy import QtWidgets
 
 from pymodaq_gui import utils as gutils
@@ -27,19 +28,41 @@ plugin_config = PluginConfig()
 EXTENSION_NAME = 'Absorption'
 CLASS_NAME = 'Absorption'
 
+RAW             = 0
+WITH_BACKGROUND = 1
+ABSORPTION      = 2
+
+
 class Absorption(CustomExt):
 
-    params = [
-        {'name': 'device_params', 'title': 'Device parameters', 'type': 'group',
-         'children': [
-             {'name': 'integration_time', 'title': 'Integration Time [ms]',
-              'type': 'float', 'min': 0.001, 'max': 10000, 'value': 50,
-              'tip': 'Integration time in seconds'},
-             {'name': 'averaging', 'title': 'Averaging',
-              'type': 'int', 'min': 1, 'max': 1000, 'value': 10,
-              'tip': 'Software Averaging'},
-             ]
-         },
+    measurement_modes = { 'Raw': RAW, 'Background Subtracted': WITH_BACKGROUND,
+                          'Absorption': ABSORPTION }
+    mode_names = list(measurement_modes.keys())
+
+    device_params = [
+        {'name': 'integration_time', 'title': 'Integration Time [ms]',
+         'type': 'float', 'min': 0.001, 'max': 10000, 'value': 50,
+         'tip': 'Integration time in seconds'},
+        {'name': 'averaging', 'title': 'Averaging',
+         'type': 'int', 'min': 1, 'max': 1000, 'value': 10,
+         'tip': 'Software Averaging'},
+        ]
+
+    application_params = [
+        {'name': 'measurement_mode', 'title': 'Measurement Mode',
+         'type': 'list', 'limits': mode_names, 'value': mode_names[0],
+         'tip': 'Measurement Mode'},
+        {'name': 'back_averaging', 'title': 'Background Averaging',
+         'type': 'int', 'min': 1, 'max': 1000, 'value': 100,
+         'tip': 'Background Software Averaging'},
+        {'name': 'ref_averaging', 'title': 'Reference Averaging',
+         'type': 'int', 'min': 1, 'max': 1000, 'value': 100,
+         'tip': 'Reference Software Averaging'},
+        ]
+
+    params = application_params + [
+         {'name': 'device_params', 'title': 'Device parameters', 'type': 'group',
+         'children': device_params },
         ]
 
     def __init__(self, parent: gutils.DockArea, dashboard):
@@ -57,16 +80,37 @@ class Absorption(CustomExt):
         self.docks['settings'] = Dock('Application Settings')
         self.dockarea.addDock(self.docks['settings'])
         self.docks['settings'].addWidget(self.settings_tree)
-        
-        self.spectrum_label = DockLabel("Raw Data")
+
+        self.spectrum_label = DockLabel("Current Data")
         spectrum_dock = Dock('Data', label=self.spectrum_label)
         self.docks['spectrum'] = \
-            self.dockarea.addDock(spectrum_dock), "right",
+            self.dockarea.addDock(spectrum_dock, "right",
                                   self.docks['settings'])
         spectrum_widget = QtWidgets.QWidget()
         self.spectrum_viewer = Viewer1D(spectrum_widget)
         self.spectrum_viewer.toolbar.hide()
         spectrum_dock.addWidget(spectrum_widget)
+
+        # plot for raw spectrum data and reference 
+        raw_data_dock = Dock('Raw Data')
+        self.docks['raw-data'] = \
+            self.dockarea.addDock(raw_data_dock, "bottom",
+                                  self.docks['settings'])
+        raw_data_widget = QtWidgets.QWidget()
+        self.raw_data_viewer = Viewer1D(raw_data_widget)
+        self.raw_data_viewer.toolbar.hide()
+
+        raw_data_dock.addWidget(raw_data_widget)
+
+        # plot for background 
+        background_dock = Dock('Background')
+        self.docks['background'] = \
+            self.dockarea.addDock(background_dock, "bottom",
+                                  self.docks['raw-data'])
+        background_widget = QtWidgets.QWidget()
+        self.background_viewer = Viewer1D(background_widget)
+        background_dock.addWidget(background_widget)
+        self.background_viewer.toolbar.hide()
 
     def read_settings(self, qt_settings):
         # @PyMoDAQxperts: is there a better better way of doing?
@@ -79,10 +123,23 @@ class Absorption(CustomExt):
             except: # pyqtgraph's state restoring is not very fail safe
                 # erease inconsistent settings in case pyqtgraph trips
                 qt_settings.setValue("dockarea", None)
+
+        for param in self.device_params:
+            self.settings.child('device_params')[param['name']] = \
+                qt_settings.value(param['name'], param['value'])
+        for param in self.application_params:
+            self.settings[param['name']] = \
+                qt_settings.value(param['name'], param['value'])
         
     def write_settings(self, qt_settings):
         qt_settings.setValue("geometry", self.mainwindow.saveGeometry())
         qt_settings.setValue("dockarea", self.dockarea.saveState())
+        for param in self.device_params:
+            name = param['name']
+            qt_settings.setValue(name, 
+                                 self.settings.child('device_params')[name])
+        for param in self.application_params:
+            qt_settings.setValue(name, self.settings[param['name']])
 
     def quit_fun(self):
         self.write_settings(self.qt_settings)
@@ -104,7 +161,24 @@ class Absorption(CustomExt):
 
     def take_data(self, data: DataToExport):
         spectro_data = data.get_data_from_dim('Data1D')[0]
-        self.spectrum_viewer.show_data(spectro_data)
+        self.n_samples = self.accumulate_data(spectro_data[0], self.n_samples)
+        if self.n_samples < self.n_average:
+            return
+
+        if self.n_average < 2:
+            self.spectrum_viewer.show_data(spectro_data)
+            return
+
+        mean, error = \
+            self.average_data(self.sum_data, self.squares_data,
+                              self.n_samples)
+        self.show_data(mean, error, 'raw')
+        self.n_samples = 0
+
+    def show_data(self, mean, error, name, raw=None, reference=None):
+        dfp = DataFromPlugins(name=name, data=[mean, error], dim='Data1D',
+                              labels=[name, 'error'], axes=[self.x_axis])
+        self.spectrum_viewer.show_data(dfp)
 
     def do_things_after_preset_set(self, preset_name: str):
         self.modules_manager.actuators_all = \
@@ -126,13 +200,21 @@ class Absorption(CustomExt):
                         "Acquire", checkable=False, toolbar=self.toolbar)
         self.add_action('stop', 'Stop', 'stop2',
                         "Stop", checkable=False, toolbar=self.toolbar)
+        self.add_action('background', 'Take Background', 'brightness_3',
+                        "Take Background", checkable=False, toolbar=self.toolbar)
+        self.add_action('reference', 'Take Reference', 'lightbulb',
+                        "Take Reference", checkable=False, toolbar=self.toolbar)
         self._actions["stop"].setEnabled(False)
 
     def connect_things(self):
         self.connect_action('acquire', self.start_acquiring)
         self.connect_action('stop', self.stop_acquiring)
+        #self.connect_action('background', self.take_background)
+        #self.connect_action('reference', self.take_reference)
 
     def start_acquiring(self):
+        self.n_samples = 0
+        self.n_average = self.settings.child('device_params')['averaging']
         self._actions["acquire"].setEnabled(False)
         self._actions["stop"].setEnabled(True)
         self.detector.grab()
@@ -150,7 +232,6 @@ class Absorption(CustomExt):
         #self.affect_to('quit', file_menu)
 
     def value_changed(self, param):
-        return
         if param.name() == "integration_time":
             self.detector.settings.child('detector_settings',
                                          'integration_time') \
