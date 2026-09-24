89e915b677ab308ba070538727ae968058564a39
Bernhard Lang <82966268+bernhardlang@users.noreply.github.com>
2026-09-24 15:48:49 +0200

    bare-extension

--- src/pymodaq_plugins_tutorial_extension/extensions/absorption_extension.py
+++ src/pymodaq_plugins_tutorial_extension/extensions/absorption_extension.py
@@ -3,12 +3,8 @@ from qtpy import QtWidgets
 from pymodaq_gui import utils as gutils
 from pymodaq_utils.config import Config, ConfigError
 from pymodaq_utils.logger import set_logger, get_module_name
-
 from pymodaq.extensions.utils import CustomExt
-
-
-# todo: replace here *pymodaq_plugins_template* by your plugin package name
-from pymodaq_plugins_template.utils import Config as PluginConfig
+from pymodaq_plugins_tutorial_extension.utils import Config as PluginConfig
 
 logger = set_logger(get_module_name(__file__))
 
@@ -16,46 +12,28 @@ main_config = Config()
 plugin_config = PluginConfig()
 
 # todo: modify this as you wish
-EXTENSION_NAME = 'MY_EXTENSION_NAME'  # the name that will be displayed in the extension list in the
-# dashboard
-CLASS_NAME = 'CustomExtensionTemplate'  # this should be the name of your class defined below
+EXTENSION_NAME = 'Absorption'
+CLASS_NAME = 'AbsorptionExtension'
 
 
 # todo: modify the name of this class to reflect its application and change the name in the main
 # method at the end of the script
-class CustomExtensionTemplate(CustomExt):
-
-    # todo: if you wish to create custom Parameter and corresponding widgets. These will be
-    # automatically added as children of self.settings. Morevover, the self.settings_tree will
-    # render the widgets in a Qtree. If you wish to see it in your app, add is into a Dock
-    params = []
+class AbsorptionExtension(CustomExt):
 
     def __init__(self, parent: gutils.DockArea, dashboard):
         super().__init__(parent, dashboard)
-
-        # info: in an extension, if you want to interact with ControlModules you have to use the
-        # object: self.modules_manager which is a ModulesManager instance from the dashboard
-
         self.setup_ui()
 
     def setup_docks_and_widgets(self):
-        """Mandatory method to be subclassed to setup the docks layout
+        self.create_dashboard_toolbar()
 
-        Examples
-        --------
-        >>>self.docks['ADock'] = gutils.Dock('ADock name')
-        >>>self.dockarea.addDock(self.docks['ADock'])
-        >>>self.docks['AnotherDock'] = gutils.Dock('AnotherDock name')
-        >>>self.dockarea.addDock(self.docks['AnotherDock'''], 'bottom', self.docks['ADock'])
-
-        See Also
-        --------
-        pyqtgraph.dockarea.Dock
-        """
-        # todo: create docks and add them here to hold your widgets
-        # reminder, the attribute self.settings_tree will  render the widgets in a Qtree.
-        # If you wish to see it in your app, add is into a Dock
-        raise NotImplementedError
+        self.spectrum_label = DockLabel("Raw Data")
+        spectrum_dock = Dock('Data', label=self.spectrum_label)
+        self.docks['spectrum'] = self.dockarea.addDock(spectrum_dock)
+        spectrum_widget = QtWidgets.QWidget()
+        self.spectrum_viewer = Viewer1D(spectrum_widget)
+        self.spectrum_viewer.toolbar.hide()
+        spectrum_dock.addWidget(spectrum_widget)
 
     def setup_menus_and_toolbars(self, menubar: QtWidgets.QMenuBar = None):
         """Non mandatory method to be subclassed in order to create a menubar
@@ -79,6 +57,7 @@ class CustomExtensionTemplate(CustomExt):
         self.create_dashboard_toolbar(add_break=False)
 
     def setup_actions(self):
+        return
         """Method where to create actions to be subclassed. Mandatory
 
         Examples
@@ -96,6 +75,7 @@ class CustomExtensionTemplate(CustomExt):
         raise NotImplementedError(f'You have to define actions here')
 
     def connect_things(self):
+        return
         """Connect actions and/or other widgets signal to methods"""
         raise NotImplementedError
 
